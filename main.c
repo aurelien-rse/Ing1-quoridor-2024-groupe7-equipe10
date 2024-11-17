fe3d16f953bf5941f858b52ec333938b5c1557f9
Mxr27 <romeo.morelli@edu.ece.fr>
2024-11-17 11:32:22 +0100

    Menu fait ;

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
-
-int main(void)
-{
-    printf("Hello, World!\n");
-    return 0;
+void affiche_menu( int menu) {
+    printf("1.Lancer une partie \n");
+    printf("2.Reprendre une partie \n");
+    printf("3.Affiche aide \n");
+    printf("4.Afficher scores et jouueurs\n");
+    printf("5.Quitter \n");
+    scanf(" %d",&menu);
+    switch(menu){
+        case 1:
+            printf("1.Lancer une partie \n");
+        break;
+        case 2:
+            printf("2.Reprendre une partie \n");
+        break;
+        case 3:
+            printf("3.Affiche aide \n");
+        break;
+        case 4:
+            printf("4.Afficher scores et jouueurs \n");
+        break;
+        case 5:
+            break;
+    }
 }
