8dd08cd3946549fb9393914689ff5cc83e40dca7
Ahmet-sezer <ahmet.sezer@edu.ece.fr>
2024-11-18 14:04:03 +0100

    main.c

--- main.c
+++ main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello, World!\n");
+
+typedef struct {
+    int x; // Coordonnée horizontale
+    int y; // Coordonnée verticale
+} Position;
+
+
+void deplacerJoueur(Position *joueur) {
+    char direction;
+    printf("Entrez une direction ('z': haut, 's': bas, 'q': gauche, 'd': droite) : ");
+    scanf(" %c", &direction); 
+
+    switch (direction) {
+        case 'z': 
+            joueur->y -= 1;
+        break;
+        case 's': 
+            joueur->y += 1;
+        break;
+        case 'q': 
+            joueur->x -= 1;
+        break;
+        case 'd': 
+            joueur->x += 1;
+        break;
+        default:
+            printf("Commande invalide !\n");
+        return;
+    }
+
+    
+    printf("Nouvelle position : (%d, %d)\n", joueur->x, joueur->y);
+}
+
+int main() {
+   
+    Position joueur = {0, 0};
+
+    printf("Position initiale : (%d, %d)\n", joueur.x, joueur.y);
+
+    
+    deplacerJoueur(&joueur);
+
     return 0;
 }
