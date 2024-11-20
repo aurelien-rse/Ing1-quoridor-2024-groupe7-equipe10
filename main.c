d26bee1f1464f428be77863b9a2c3b354904b410
aurelien-rse <aurelien.risse@edu.ece.fr>
2024-11-20 14:54:25 +0100

    Update n°1 main.c

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello, World!\n");
+#define FICHIER_SCORES "scores.txt"
+
+void afficherScores() {
+    FILE *fichier = fopen(FICHIER_SCORES, "r");
+    if (!fichier) {
+        printf("Erreur : Impossible d'ouvrir le fichier des scores (%s).\n", FICHIER_SCORES);
+        return;
+    }
+
+    printf("\n    Scores finaux des joueurs    \n");
+    char pseudo[50];
+    int score;
+
+    while (fscanf(fichier, "%s %d", pseudo, &score) == 2) {
+        printf("%s : %d points\n", pseudo, score);
+    }
+    printf("                      \n");
+
+    fclose(fichier);
+}
+
+int main() {
+    afficherScores();
     return 0;
 }
