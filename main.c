ef0f16cb6f2b275df608f2949527c287799fb4a2
aurelien-rse <aurelien.risse@edu.ece.fr>
2024-11-20 17:31:18 +0100

    Fonction 3 du Menu : Afficher l'aide
    A additionner avec le code du menu (Pour le cas 3)

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello, World!\n");
-    return 0;
+void afficherAide() {
+    printf("\n    Aide : Regles du jeu Quoridor     \n");
+    printf("1. Le but est d'atteindre en premier la ligne opposee au point de depart.\n");
+    printf("2. Les joueurs jouent chacun leur tour, avec comme possibilite :\n");
+    printf("   - Deplacer son pion d’une case (avant, arrière, gauche, droite).\n");
+    printf("   - Poser une barriere pour ralentir les adversaires.\n");
+    printf("3. Les barrieres ne doivent pas bloquer totalement les chemins.\n");
+    printf("4. Le premier a atteindre la ligne en face gagne 5 points.\n");
+    printf("5. En cas de blocage total, il n’y a pas de gagnant.\n");
+    printf("6. Chaque partie met à jour les scores des joueurs.\n");
+    printf("7. Le deplacement ne peut pas traverser une barrière ou sortir du plateau.\n");
 }
